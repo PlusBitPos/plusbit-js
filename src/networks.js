41711f12629fa7f9140ad50f322518c8a5716ee3
wkibbler <38703970+wkibbler@users.noreply.github.com>
2019-07-22 18:18:54 +0100

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -14,6 +14,17 @@ module.exports = {
     wif: 0x3F,
     coin: coins.HASH
   },
+  qbc: {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x3a,
+    scriptHash: 0x5,
+    wif: 0xba,
+    coin: coins.QBC
+  },
   xuez: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
     bip32: {
